d41a18938a901a7ec60e192265e35584aa73ccf9
lami216 <iiyad21694@gmail.com>
2025-11-11 17:24:37 -0100

    Increase hero slide image size limit

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,8 @@ const app = express();
 const PORT = process.env.PORT || 10003;
 const __dirname = path.resolve();
 
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json({ limit: "15mb" }));
+app.use(express.urlencoded({ limit: "15mb", extended: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
